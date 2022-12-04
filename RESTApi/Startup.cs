b02ca0dbf174b23809fa976b03b39e71847c5291
TinNT49 <42561858+TinNT49@users.noreply.github.com>
2022-12-04 22:04:33 +0700

    20221204_Commit code after creating database and connecting it to application

---
+++ RESTApi/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using RESTApi.Models;
+using System;
+using RESTApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RESTApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configure(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("StoreDB");
+            services.AddDbContextPool<StoreDBContext>(options => options.UseSqlServer(connection));
+        }
+    }
+}
